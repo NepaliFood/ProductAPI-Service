41e649f495daafe9b390ce4a68d76329ce01e586
Naresh Kumar Rai <n.rai@student.fontys.nl>
2022-04-21 16:54:37 +0200

    Seeds Products to Db

--- ProductAPI/ProductAPI/DBContext/ApplicationDbContext.cs
+++ ProductAPI/ProductAPI/DBContext/ApplicationDbContext.cs
@@ -15,7 +15,7 @@ namespace ProducAPI.DBContext
         }
         public DbSet<Product> Products { get; set; }
 
-       /* protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
@@ -24,7 +24,7 @@ namespace ProducAPI.DBContext
                 ProductId = 1,
                 Name = "Butter Chicken",
                 Category = "Main",
-                ImageFile = "",
+                ImageFile = "https://freeimage.host/i/VNmlwu",
                 Description = "Aromatic golden chicken pieces in an incredible curry sauce.",
                 Price = 18,
                 SpicyLevel = "Mild"
@@ -36,7 +36,7 @@ namespace ProducAPI.DBContext
                 ProductId = 2,
                 Name = "Vindaloo",
                 Category = "Main",
-                ImageFile = "",
+                ImageFile = "https://freeimage.host/i/VNmcue",
                 Description = "A standard element of Goan cuisine derived from the Portuguese carne de vinha d'alhos (meat in garlic marinade).",
                 Price = 16,
                 SpicyLevel = "High"
@@ -47,7 +47,7 @@ namespace ProducAPI.DBContext
                 ProductId = 3,
                 Name = "Lamb Roghanjosh",
                 Category = "Curry",
-                ImageFile = "",
+                ImageFile = "https://freeimage.host/i/VNma99",
                 Description = "An indian lamb curry with a heady combination of intense spices in a creamy tomato curry sauce.",
                 Price = 18,
                 SpicyLevel = "Medium"
@@ -58,7 +58,7 @@ namespace ProducAPI.DBContext
                 ProductId = 4,
                 Name = "Paneer Butter Masala",
                 Category = "Main",
-                ImageFile = "",
+                ImageFile = "https://freeimage.host/i/VNm787",
                 Description = "A rich & creamy curry made with paneer, spices, onions, tomatoes, cashews and butter.",
                 Price = 18,
                 SpicyLevel = "Mild"
@@ -69,22 +69,22 @@ namespace ProducAPI.DBContext
                 ProductId = 5,
                 Name = "Chicken Madras",
                 Category = "Dessert",
-                ImageFile = "",
+                ImageFile = "https://freeimage.host/i/VNm0Zb",
                 Description = "A madras curry is a fairly hot curry with a dark red thick sauce (the colour comes from the chillies and paprika).",
                 Price = 18,
                 SpicyLevel = "Mild"
-            }); 
+            });
 
             modelBuilder.Entity<Product>().HasData(new Product
             {
                 ProductId = 6,
                 Name = "Nepalese Momo",
                 Category = "Starter",
-                ImageFile = "",
+                ImageFile = "https://freeimage.host/i/VNmEnj",
                 Description = "A dumpling made of all-purpose flour and filled with either meat or vegetables.",
                 Price = 10,
                 SpicyLevel = "Medium"
             });
-        }*/
+        }
     }
 }
